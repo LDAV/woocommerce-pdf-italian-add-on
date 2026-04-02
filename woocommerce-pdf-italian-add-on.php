8e0d3ed7947fbc013addd5044b5613ade0e4d03c
LDAV <info@ldav.it>
2026-04-02 10:37:54 +0200

    v.0.7.7

--- woocommerce-pdf-italian-add-on.php
+++ woocommerce-pdf-italian-add-on.php
@@ -12,7 +12,7 @@
 * Domain Path: /languages
 * Requires Plugins: woocommerce
 * WC requires at least: 8.0
-* WC tested up to: 10.4.3
+* WC tested up to: 10.6.2
 */
 
 //Thanks to Nicola Mustone https://gist.github.com/SiR-DanieL
